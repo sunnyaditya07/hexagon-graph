f535b0d79041ba14a7a55a7a608ea0dff14bc242
sunnyaditya07 <sunnyadityaa@gmail.com>
2024-05-17 13:17:57 +0530

    I have added a dropdown using react bootstrap library and also made a hexagon shape using css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { Dropdown, DropdownButton } from "react-bootstrap";
+import "./App.css";
 
-function App() {
+import React, { useState } from "react";
+
+const App = () => {
+  const paths = [
+    "Select path",
+    "A-B",
+    "A-C",
+    "A-D",
+    "A-E",
+    "B-C",
+    "B-D",
+    "B-E",
+    "C-D",
+    "C-E",
+    "D-E",
+  ];
+  const [selectedPath, setSelectedPath] = useState("Select path");
+
+  const handleSelect = (eventKey) => {
+    setSelectedPath(paths[eventKey]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="main-container">
+      <div className="dropdown-container">
+        <h5>Select path</h5>
+        <DropdownButton
+          id="dropdown-basic-button"
+          title={selectedPath}
+          onSelect={handleSelect}
+          variant="secondary"
         >
-          Learn React
-        </a>
-      </header>
+          {paths.map((path, index) => (
+            <Dropdown.Item key={index} eventKey={index}>
+              {path}
+            </Dropdown.Item>
+          ))}
+        </DropdownButton>
+      </div>
+      <div className="hexagon-container">
+        <div className="alphabet a">A</div>
+        <div className="alphabet b">B</div>
+        <div className="alphabet c">C</div>
+        <div className="alphabet d">D</div>
+        <div className="alphabet e">E</div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
