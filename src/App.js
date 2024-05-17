a7799a9f988daa3922c60e09cf6ef686db6a9d12
sunnyaditya07 <sunnyadityaa@gmail.com>
2024-05-17 14:51:18 +0530

    i have added the path between two points of hexagon shape, suppose if user select the path from A->B then that path will be highlighted with yellow colour, and if the user select the indirect path from A->C then the path of A-B and B-C( resultant path A->B->C ) will be highlighted with yellow colour

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import React, { useState } from "react";
 const App = () => {
   const paths = [
     "Select path",
-    "A-B",
-    "A-C",
-    "A-D",
-    "A-E",
-    "B-C",
-    "B-D",
-    "B-E",
-    "C-D",
-    "C-E",
-    "D-E",
+    "A->B",
+    "A->C",
+    "A->D",
+    "A->E",
+    "B->C",
+    "B->D",
+    "B->E",
+    "C->D",
+    "C->E",
+    "D->E",
+    "A->E->D",
+    "A->B->C",
   ];
   const [selectedPath, setSelectedPath] = useState("Select path");
 
@@ -45,6 +47,105 @@ const App = () => {
         <div className="alphabet c">C</div>
         <div className="alphabet d">D</div>
         <div className="alphabet e">E</div>
+        <svg className="line-container">
+          <line
+            id="A->B"
+            className={`${
+              selectedPath === "A->B" || selectedPath === "A->B->C"
+                ? "active-line"
+                : "line"
+            }`}
+            x1="30"
+            x2="250"
+            y1="230"
+            y2="20"
+          />
+          <line
+            id="A->C"
+            className={`${selectedPath === "A->C" ? "active-line" : "line"}`}
+            x1="130"
+            x2="250"
+            y1="460"
+            y2="20"
+          />
+          <line
+            id="A->D"
+            className={`${selectedPath === "A->D" ? "active-line" : "line"}`}
+            x1="360"
+            x2="250"
+            y1="460"
+            y2="20"
+          />
+          <line
+            id="A->E"
+            className={`${
+              selectedPath === "A->E" || selectedPath === "A->E->D"
+                ? "active-line"
+                : "line"
+            }`}
+            x1="480"
+            x2="250"
+            y1="240"
+            y2="20"
+          />
+
+          <line
+            id="B->C"
+            className={`${
+              selectedPath === "B->C" || selectedPath === "A->B->C"
+                ? "active-line"
+                : "line"
+            }`}
+            x1="30"
+            x2="240"
+            y1="210"
+            y2="800"
+          />
+          <line
+            id="B->D"
+            className={`${selectedPath === "B->D" ? "active-line" : "line"}`}
+            x1="10"
+            x2="380"
+            y1="210"
+            y2="480"
+          />
+          <line
+            id="B->E"
+            className={`${selectedPath === "B->E" ? "active-line" : "line"}`}
+            x1="50"
+            x2="480"
+            y1="230"
+            y2="230"
+          />
+          <line
+            id="C->D"
+            className={`${selectedPath === "C->D" ? "active-line" : "line"}`}
+            x1="120"
+            x2="380"
+            y1="480"
+            y2="480"
+          />
+          <line
+            id="C->E"
+            className={`${selectedPath === "C->E" ? "active-line" : "line"}`}
+            x1="120"
+            x2="480"
+            y1="480"
+            y2="220"
+          />
+          <line
+            id="D->E"
+            className={`${
+              selectedPath === "D->E" || selectedPath === "A->E->D"
+                ? "active-line"
+                : "line"
+            }`}
+            x1="380"
+            x2="480"
+            y1="480"
+            y2="220"
+          />
+        </svg>
       </div>
     </div>
   );
